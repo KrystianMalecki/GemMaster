9a1cee928c661cca34a4286c0373fcb59c82c134
NitroxPL <pawelpardel2@wp.pl>
2021-01-20 16:34:31 +0100

    test player and TV

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask platformLayerMask;
+
+    public Rigidbody2D rbBody;
+    public float jump;
+    private float horizontalMove;
+    public bool facingRight = true;
+    public float speed = 1f;
+    public BoxCollider2D col;
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = gameObject.transform.localScale;
+        scale.x *= -1;
+        gameObject.transform.localScale = scale;
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D rayCastHit = Physics2D.Raycast(col.bounds.center, Vector2.down, col.bounds.extents.y + 0.1f, platformLayerMask);
+        return rayCastHit.collider != null;
+    }
+
+    void Update()
+    {
+        
+            horizontalMove = Input.GetAxis("Horizontal");
+
+            if (horizontalMove > 0.4f || horizontalMove < -0.4f)
+            {
+                transform.position += transform.right * (speed * Time.deltaTime * horizontalMove);
+            }
+
+            if (horizontalMove < 0 && facingRight)
+            {
+                Flip();
+            }
+
+            if (horizontalMove > 0 && !facingRight)
+            {
+                Flip();
+            }
+
+            if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.UpArrow)) && IsGrounded())
+            {
+                rbBody.velocity = Vector2.up * jump;
+            }
+
+    }
+}
---
+++ Assets/Scripts/TVscript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVscript : MonoBehaviour
+{
+    public Transform player;
+    public PlayerScript playerScript;
+    float xVelocity = 0.0f, yVelocity = 0.0f, newPositionx, newPositiony, closestDistance;
+    public float speedx, speedy;
+    private GameObject closest;
+
+    void Update()
+    {
+        FindClosestEnemy();
+
+        if (closestDistance < 70f)
+        {
+            newPositionx = Mathf.SmoothDamp(transform.position.x, closest.transform.position.x, ref xVelocity, 0.2f);
+            newPositiony = Mathf.SmoothDamp(transform.position.y, closest.transform.position.y + 1.5f, ref yVelocity, 0.2f);
+
+            Debug.DrawLine(transform.position, closest.transform.position, Color.red);
+        }
+        else
+        {
+            if (Input.GetAxis("Horizontal") > 0.4f || Input.GetAxis("Horizontal") < -0.4f)
+            {
+                if (playerScript.facingRight)
+                {
+                    newPositionx = Mathf.SmoothDamp(transform.position.x, player.position.x + 6f, ref xVelocity, speedx);
+                    newPositiony = Mathf.SmoothDamp(transform.position.y, player.position.y + 2f, ref yVelocity, speedy);
+                }
+                else
+                {
+                    newPositionx = Mathf.SmoothDamp(transform.position.x, player.position.x - 6f, ref xVelocity, speedx);
+                    newPositiony = Mathf.SmoothDamp(transform.position.y, player.position.y + 2f, ref yVelocity, speedy);
+                }
+            }
+            else
+            {
+                newPositionx = Mathf.SmoothDamp(transform.position.x, player.position.x, ref xVelocity, speedx, 2f);
+                newPositiony = Mathf.SmoothDamp(transform.position.y, player.position.y + 2f, ref yVelocity, speedy);
+            }
+
+            Debug.DrawLine(transform.position, closest.transform.position, Color.green);
+        }
+
+        transform.position = new Vector3(newPositionx, newPositiony, transform.position.z);
+    }
+
+    void FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Programmable");
+        closest = null;
+        closestDistance = Mathf.Infinity;
+        Vector3 position = player.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < closestDistance)
+            {
+                closest = go;
+                closestDistance = curDistance;
+            }
+        }
+    }
+}
