e546b998ff695f1d05855a28a467d91fdd8f5e05
TheShadowKnight <35604794+theshadowknight@users.noreply.github.com>
2021-01-21 16:12:27 +0100

    Moving To BAP 1/2

--- Assets/FollowerMovement.cs
+++ Assets/FollowerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class FollowerMovement : MonoBehaviour
 {
     public PlayerScript playerScirpt;
-    public Transform targetPoint;
+    public BlockAccesPoint targetPoint;
+    public List<BlockAccesPoint> BAPsInRange = new List<BlockAccesPoint>();
+    public bool moveToBAP;
     public float moveSpeed;
+    public bool attachedToBAP;
     public Vector2 moveDir => targetPosition -new Vector2( transform.position.x, transform.position.y);
     public float targetDistSqr => moveDir.sqrMagnitude;
 
@@ -14,15 +17,71 @@ public class FollowerMovement : MonoBehaviour
     {
         get
         {
-            return playerScirpt.lightPoint.position;
+            if (moveToBAP)
+            {
+                return targetPoint.transform.position;
+            }
+            else
+            {
+                return playerScirpt.lightPoint.position;
+
+            }
         }
     }
     public void Update()
     {
-        if (targetDistSqr > 0.2f)
+        if (!attachedToBAP)
         {
-            transform.Translate(moveDir.normalized * moveSpeed);
-            transform.localScale = new Vector3(playerScirpt.facingRight?-2:2,2,2);
+            if (targetDistSqr > 0.2f)
+            {
+                transform.Translate(moveDir.normalized * moveSpeed);
+                transform.localScale = new Vector3(playerScirpt.facingRight ? -2 : 2, 2, 2);
+            }
+            if (moveToBAP)
+            {
+                if (targetDistSqr < 0.2f)
+                {
+                    targetPoint.Attach(this);
+                }
+            }
         }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!attachedToBAP)
+            {
+                targetPoint = GetClosestBAP();
+                moveToBAP = true;
+            }
+            else
+            {
+                targetPoint.Deattach();
+                moveToBAP = false;
+            }
+        }
+       
+    }
+
+    public BlockAccesPoint GetClosestBAP()
+    {
+        float currDist = 10000f;
+        int currId = 0;
+        for (int i = 0; i < BAPsInRange.Count; i++)
+        {
+            float dist = (BAPsInRange[i].transform.position - transform.position).sqrMagnitude;
+            if (currDist>=dist)
+            {
+                currDist = dist;
+                currId = i;
+            }
+        }
+        return BAPsInRange[currId];
+    }
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("BAP")) {
+            BAPsInRange.Add(collision.GetComponent<BlockAccesPoint>());
+        }
+
     }
 }
