3e560fedde41d414fafa3abb1fec1b7b22083d18
TheShadowKnight <35604794+theshadowknight@users.noreply.github.com>
2021-01-21 15:45:17 +0100

    tv anim cool and movement

---
+++ Assets/FollowerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerMovement : MonoBehaviour
+{
+    public PlayerScript playerScirpt;
+    public Transform targetPoint;
+    public float moveSpeed;
+    public Vector2 moveDir => targetPosition -new Vector2( transform.position.x, transform.position.y);
+    public float targetDistSqr => moveDir.sqrMagnitude;
+
+    public Vector2 targetPosition
+    {
+        get
+        {
+            return playerScirpt.lightPoint.position;
+        }
+    }
+    public void Update()
+    {
+        if (targetDistSqr > 0.2f)
+        {
+            transform.Translate(moveDir.normalized * moveSpeed);
+            transform.localScale = new Vector3(playerScirpt.facingRight?-2:2,2,2);
+        }
+    }
+}
