9d5c9f75b1f842f83777ac21aebf2db902c98e7d
TheShadowKnight <35604794+theshadowknight@users.noreply.github.com>
2021-01-24 16:18:38 +0100

    New anim for BAP

--- Assets/BAP/BlockAccesPoint.cs
+++ Assets/BAP/BlockAccesPoint.cs
@@ -1,30 +1,45 @@
 ﻿using ConditionalAttribute;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
+
 public class BlockAccesPoint : MonoBehaviour
 {
     [SerializeField]
     private bool showMoreData;
     [ConditionalField("showMoreData")] public FollowerMovement attached;
     [ConditionalField("showMoreData")] public Animator animator;
+    public UnityEvent OnAttach;
+    public UnityEvent OnDeattach;
+    public UnityEvent OnExecute;
+
     public void Attach(FollowerMovement fm)
     {
         attached = fm;
         attached.Attach();
-        animator.Play("Attach");
+     //   animator.Play("Attach");
+        animator.Play("Attach new");
+
+        OnAttach.Invoke();
     }
     public void Execute()
     {
+        OnExecute.Invoke();
 
     }
     public void Deattach()
     {
         StartCoroutine(deattach());
+        OnDeattach.Invoke();
     }
     IEnumerator deattach()
     {
-        animator.Play("Deattach");
-        yield return new WaitForSeconds(50f / 60f);
+     //   animator.Play("Deattach");
+            animator.Play("Deattach new");
+
+        //   yield return new WaitForSeconds(50f / 60f);
+           yield return new WaitForSeconds(60f / 60f);
+
         attached.Deattach(); 
         attached = null;
 
