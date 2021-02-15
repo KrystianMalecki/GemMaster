45fb1d6ecaec789b1b044f9fc703a404c356e9f4
TheShadowKnight <35604794+theshadowknight@users.noreply.github.com>
2021-02-15 14:04:34 +0100

    Eye Movement base

---
+++ Assets/EyeMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeMovement : MonoBehaviour
+{
+    public Transform mainTransform;
+    Vector2 eyeXrange = new Vector2(-0.125f, -0.0625f);
+    Vector2 eyeYrange = new Vector2(0.125f, 0);
+    public void Update()
+    {
+        transform.localPosition = new Vector3(
+           (mainTransform.localScale.x==-1? (-0.0625f*3) : 0f)+
+           mainTransform.localScale.x * Mathf.Lerp(eyeXrange.x, eyeXrange.y, Input.mousePosition.x / Camera.main.pixelWidth),
+                        Mathf.Lerp(eyeYrange.x, eyeYrange.y,1- Input.mousePosition.y / Camera.main.pixelHeight),0);
+    }
+}
