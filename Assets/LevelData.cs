88776618e0b379261211084cb581feba88d9a77b
TheShadowKnight <35604794+theshadowknight@users.noreply.github.com>
2021-04-07 22:19:06 +0200

    .

---
+++ Assets/LevelData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum LevelTag { }
+[System.Serializable]
+public class LevelData
+{
+    public LevelTag tagName;
+    public GameObject mainPart;
+    //TODO: add spawning enemies
+}
