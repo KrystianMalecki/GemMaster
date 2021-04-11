010a9bfd0c55ea81943593a0a32d4eb6ea8e3a8d
TheShadowKnight <35604794+theshadowknight@users.noreply.github.com>
2021-04-11 21:00:54 +0200

    sound changes

---
+++ Assets/Scripts/Maker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maker
+{
+    [UnityEditor.MenuItem("addsoundpoints", menuItem = "Tools/addsoundpoints")]
+    public static void addsoundpoints()
+    {
+        AudioSource[] asses = GameObject.FindObjectsOfType<AudioSource>(true);
+
+        for (int i = 0; i < asses.Length; ++i)
+        {
+            SoundPoint sp = asses[i].gameObject.AddComponent<SoundPoint>();
+            sp.audioSource = asses[i];
+            sp.isMusic = false;
+        }
+    }
+}
